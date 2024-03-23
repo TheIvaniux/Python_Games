ecc9ded647af85b8a3ebd7797133b6e632c57a2d
ikergr <a01746978@tec.mx>
2024-03-22 20:03:30 -0600

    Función de Update de Taps y centrado de números

--- memory.py
+++ memory.py
@@ -19,7 +19,7 @@ tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
 available_colors = ['blue', 'green', 'red', 'purple', 'orange', 'yellow', 'cyan', 'magenta', 'lime', 'pink', 'teal', 'olive', 'navy', 'maroon', 'brown', 'aquamarine', 'crimson', 'indigo', 'gold', 'silver', 'lavender', 'plum', 'coral', 'peru', 'orchid', 'skyblue', 'violet', 'khaki', 'salmon', 'tan', 'black', 'gray']
-
+tapcounter = 0
 
 def square(x, y):
     """Draw white square with black outline at (x, y)."""
@@ -56,7 +56,8 @@ def tap(x, y):
         hide[mark] = False
         state['mark'] = None
 
-
+    global tapcounter
+    tapcounter += 1
 def draw():
     """Draw image and tiles."""
     clear()
@@ -70,14 +71,17 @@ def draw():
             square(x, y)
 
     mark = state['mark']
-
+    goto(0,-250)
+    write('Number of taps: ' + str(tapcounter), align='center', font=('Arial', 24, 'normal'))
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        goto(x + 24, y+2)
         #Define a color for each number
         color(available_colors[tiles[mark]])
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        
+        #Center the number in the square
+        write(tiles[mark], align='center',font=('Arial', 30, 'normal'))
     
     #Know if the image was completed    
     if all(not hide[count] for count in range(64)):
